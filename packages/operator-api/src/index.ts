8cf79b3220e1a45da359d3cdfaf683478f6e2d34
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 19:50:10 -0400

    feat(operator-api): export live run query surface

--- packages/operator-api/src/index.ts
+++ packages/operator-api/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./audit.js";
 export * from "./auth.js";
 export * from "./operations.js";
+export * from "./runs.js";
 export * from "./server.js";
 export * from "./types.js";
 export * from "./validation.js";
