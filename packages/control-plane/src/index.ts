c4ff2bb3220908d95fdb5efce1a01d76d93c000c
Agustealo Johnson <agustealo@gmail.com>
2026-09-22 19:32:51 -0400

    feat(control-plane): export query helpers

--- packages/control-plane/src/index.ts
+++ packages/control-plane/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./control-plane.js";
 export * from "./json.js";
+export * from "./query.js";
 export * from "./schedule.js";
 export * from "./store.js";
 export * from "./types.js";
