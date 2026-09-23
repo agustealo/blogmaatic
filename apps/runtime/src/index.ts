ebab25070157f797ca447f45ac69300da981de52
Agustealo Johnson <agustealo@gmail.com>
2026-09-23 13:11:44 -0400

    feat(runtime): export publication group manager

--- apps/runtime/src/index.ts
+++ apps/runtime/src/index.ts
@@ -4,5 +4,6 @@ export * from "./control-room-server.js";
 export * from "./credentials.js";
 export * from "./init.js";
 export * from "./processes.js";
+export * from "./publication-group-manager.js";
 export * from "./runtime.js";
 export * from "./scheduler.js";
